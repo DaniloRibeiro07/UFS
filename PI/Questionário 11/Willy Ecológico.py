f574e430eccda8af30cb82c76401066e049d93c6
DaniloRibeiro07 <danilo.ribeirodr01@gmail.com>
2021-10-19 00:13:12 -0300

    Completo

--- PI/Questionário 11/Willy Ecológico.py
+++ PI/Questionário 11/Willy Ecológico.py
@@ -3,12 +3,16 @@ array_arvores=[int(i) for i in input().split()]
 
 array_arvores.sort()
 menor_tamanho=array_arvores[-1]
-soma=0
+lista_altura=list()
+lista_madeiracomida=list()
 for i in range(n_arvores):
+    madeira_comida=0
     for n in range(n_arvores):
         if array_arvores[n]-array_arvores[i]>0:
-           soma+=array_arvores[i]-array_arvores[n]
-    if menor_tamanho>soma and soma>=tamanho:
-        menor_tamanho=array_arvores[i]
-        
-print(menor_tamanho)
+           madeira_comida+=array_arvores[n]-array_arvores[i]
+    if madeira_comida-tamanho<0:
+        continue
+    lista_altura.append(array_arvores[i])
+    lista_madeiracomida.append(madeira_comida)
+
+print(lista_altura[lista_madeiracomida.index(min(lista_madeiracomida))])
