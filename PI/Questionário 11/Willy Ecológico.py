85f3f23715e69d217b4a492d4ae1eb6a96cf5960
DaniloRibeiro07 <danilo.ribeirodr01@gmail.com>
2021-10-22 18:44:46 -0300

    Completo

--- PI/Questionário 11/Willy Ecológico.py
+++ PI/Questionário 11/Willy Ecológico.py
@@ -1,18 +1,17 @@
-n_arvores,tamanho=[int(i) for i in input().split()]
-array_arvores=[int(i) for i in input().split()]
+def verificar_madeira_conseguida(quantidade_arvores,arvores,madeira_necessaria):
+    lista_madeira_conseguida=list()
+    for i in range(quantidade_arvores):
+        madeira_conseguida=0
+        for n in range(i+1,quantidade_arvores):
+            madeira_conseguida+=arvores[n]-arvores[i]
+        lista_madeira_conseguida.append(madeira_conseguida)
+        
+    for i in range(quantidade_arvores-1,-1,-1):
+        if lista_madeira_conseguida[i]>=madeira_necessaria:
+            return i
 
-array_arvores.sort()
-menor_tamanho=array_arvores[-1]
-lista_altura=list()
-lista_madeiracomida=list()
-for i in range(n_arvores):
-    madeira_comida=0
-    for n in range(n_arvores):
-        if array_arvores[n]-array_arvores[i]>0:
-           madeira_comida+=array_arvores[n]-array_arvores[i]
-    if madeira_comida-tamanho<0:
-        continue
-    lista_altura.append(array_arvores[i])
-    lista_madeiracomida.append(madeira_comida)
-
-print(lista_altura[lista_madeiracomida.index(min(lista_madeiracomida))])
+quantidade_arvores,madeira_necessaria=[int(i) for i in input().split()]
+arvores=[int(i) for i in input().split()]
+arvores.sort()#ordenar tamanho arvore
+posicao=verificar_madeira_conseguida(quantidade_arvores,arvores,madeira_necessaria)
+print(arvores[posicao])
