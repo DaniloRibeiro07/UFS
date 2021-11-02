0e742f194254eea8599be3a41a2014789c02bad0
DaniloRibeiro07 <danilo.ribeirodr01@gmail.com>
2021-11-02 16:35:07 -0300

    Completo

---
+++ PI/Questionário 14/Camisetas.py
@@ -0,0 +1,44 @@
+quantidade_camisas=int(input())
+lista_dados_camisas=list()
+dados_camisas={}
+for i in range(quantidade_camisas):
+    aluno=input()
+    cor, tamanho=input().split()
+    lista_dados_camisas.append((cor,tamanho,aluno))
+    dados_camisas[cor]=
+lista_dados_camisas.sort()
+
+
+for linha in lista_dados_camisas:
+    dados_camisas[]
+
+'''
+3
+Maria Joao
+branco P
+Marcio Guess
+vermelho P
+Maria Jose
+branco P
+'''
+'''
+9
+Maria Jose
+branco P
+Mangojata Mancuda
+vermelho P
+Cezar Torres Mo
+branco P
+Baka Lhau
+vermelho P
+JuJu Mentina
+branco M
+Amaro Dinha
+vermelho P
+Adabi Finho
+branco G
+Severina Rigudinha
+branco G
+Carlos Chade Losna
+vermelho P
+'''
