b2fd01eca1f63a5f6cddb4f66f11dcb3ab50199f
DaniloRibeiro07 <danilo.ribeirodr01@gmail.com>
2021-11-04 15:36:07 -0300

    Completo

--- PI/Questionário 14/Academia.py
+++ PI/Questionário 14/Academia.py
@@ -5,20 +5,17 @@ for x in range(100):
     break
   senha=input()
   situacao_mensalidade=input()
-  dados_clientes[nome]=(senha,situacao_mensalidade)
+  dados_clientes[senha]=(nome,situacao_mensalidade)
 
 while True:
   senha_a_ser_testada=input()
   if senha_a_ser_testada=='-1':
     break
-
-  for nome, senha_situacao_mensalidade in dados_clientes.items():
-    if senha_situacao_mensalidade[0]==senha_a_ser_testada:
-      if senha_situacao_mensalidade[1]=='P':
-        print(nome+", seja bem-vindo(a)!")
-      else:
-        print("Não está esquecendo de algo,",nome+"? Procure a recepção!")
-
+  if senha_a_ser_testada in dados_clientes:
+    (nome,situacao_mensalidade)=dados_clientes[senha_a_ser_testada]
+    if situacao_mensalidade=='P':
+      print(nome+", seja bem-vindo(a)!")
     else:
-      print("Seja bem-vindo(a)! Procure a recepção!")
-
+      print("Não está esquecendo de algo,",nome+"? Procure a recepção!")
+  else:
+    print("Seja bem-vindo(a)! Procure a recepção!")
